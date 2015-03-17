dadd4f51cef6d695b9f0e3537adbed3c247f1a83
gabtorrespen <gabtorrespen@unal.edu.co>
2015-03-17 10:12:43 -0500

    "Update Post"

--- grails-app/domain/foro/Post.groovy
+++ grails-app/domain/foro/Post.groovy
@@ -23,4 +23,48 @@ class Post {
 
     static constraints = {
     }
+
+    String getTopic() {
+        return topic
+    }
+
+    void setTopic(String topic) {
+        this.topic = topic
+    }
+
+    Date getDateCreated() {
+        return dateCreated
+    }
+
+    void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated
+    }
+
+    Date getLastupdate() {
+        return lastupdate
+    }
+
+    void setLastupdate(Date lastupdate) {
+        this.lastupdate = lastupdate
+    }
+
+    boolean getIsAllowed() {
+        return isAllowed
+    }
+
+    void setIsAllowed(boolean isAllowed) {
+        this.isAllowed = isAllowed
+    }
+
+    void comment(){
+
+    }
+
+    void rate(){
+
+    }
+
+    void share(){
+
+    }
 }
